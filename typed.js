f1dafb71f25e392a31bff2d88fc8c8bf21be4155
Bash-Abdul <bashirtaiwo59@gmail.com>
2023-06-05 09:57:29 +0100

    added new faetures

--- typed.js
+++ typed.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.typing', {
-    strings: ['Frontend Developer', 'Mobile App Developer','Gamer 🎮'],
+    strings: ['Frontend Developer', 'Mobile App Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
